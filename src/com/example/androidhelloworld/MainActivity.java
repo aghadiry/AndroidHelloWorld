953d1ac3025b49a37360bf38668a367b367f2a14
Amir Ghadiry <aghadiry@AmirsMBP.local>
2013-05-21 17:24:25 -0500

    Added some activities and buttons

--- src/com/example/androidhelloworld/MainActivity.java
+++ src/com/example/androidhelloworld/MainActivity.java
@@ -1,15 +1,44 @@
 package com.example.androidhelloworld;
 
-import android.os.Bundle;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	Context ctx;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ctx = this;
+		Button butAct1 = (Button) findViewById(R.id.button_act_1);
+		Button butAct2 = (Button) findViewById(R.id.button_act_2);
+		
+		butAct1.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(ctx, Activity1.class);
+				startActivity(i);
+			}
+		});
+		
+		butAct2.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(ctx, Activity2.class);
+				startActivity(i);
+			}
+		});
 	}
 
 	@Override
